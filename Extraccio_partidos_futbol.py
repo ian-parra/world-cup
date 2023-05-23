c85a98d4ce8509ae132ba840ca089a8e462acebe
ian-parra <24ian2002@gmail.com>
2023-05-23 13:14:52 -0300

    Update Extraccio_partidos_futbol.py

--- Extraccio_partidos_futbol.py
+++ Extraccio_partidos_futbol.py
@@ -3,15 +3,17 @@ import requests
 import pandas as pd
 
 # data to scraper -> 1930-2022
-years = [2022]
+years = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970, 1974,
+         1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014,
+         2018, 2022]
 
 
 def get_matches(year):
-    wep = f'https://web.archive.org/web/20221115040351/https://en.wikipedia.org/wiki/2022_FIFA_World_Cup'
-    response = requests.get(wep)
-    content = response.text  # print(response.text) observación de contenido
+    web = f'https://en.wikipedia.org/wiki/{year}_FIFA_World_Cup'
+    response = requests.get(web)
+    content = response.text
     soup = BeautifulSoup(content, 'lxml')
-    matches = soup.find_all('div', class_='footballbox')  # almaceno los partidos de futbol
+    matches = soup.find_all('div', class_='footballbox')
 
     home = []
     score = []
@@ -22,17 +24,17 @@ def get_matches(year):
         score.append(match.find('th', class_='fscore').get_text())
         away.append(match.find('th', class_='faway').get_text())
 
-    dict_football = {'Local': home, 'Marcador': score, 'visitante': away}
+    dict_football = {'home': home, 'score': score, 'away': away}
     df_football = pd.DataFrame(dict_football)
-    df_football['year'] = year  # print(df_football)
+    df_football['year'] = year
     return df_football
 
 
-"""# historical_data
+# historical_data
 fifa = [get_matches(year) for year in years]
 df_fifa = pd.concat(fifa, ignore_index=True)
-df_fifa.to_csv('fifa_worldcup_historical_data2022.csv', index=False)"""
+df_fifa.to_csv("fifa_worldcup_historical_data.csv", index=False)
 
-# fixture 2022
+# fixture
 df_fixture = get_matches(2022)
-df_fixture.to_csv('fifa_worldcup_fixture2022.csv', index=False)
+df_fixture.to_csv('fifa_worldcup_fixture.csv', index=False)
