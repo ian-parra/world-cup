7dfa1a935416858b024232779d6858409c10bd95
ian-parra <24ian2002@gmail.com>
2023-05-23 13:36:00 -0300

    Update Extraccio_partidos_futbol.py

--- Extraccio_partidos_futbol.py
+++ Extraccio_partidos_futbol.py
@@ -5,11 +5,11 @@ import pandas as pd
 # data to scraper -> 1930-2022
 years = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970, 1974,
          1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014,
-         2018, 2022]
+         2018]
 
 
 def get_matches(year):
-    web = f'https://en.wikipedia.org/wiki/{year}_FIFA_World_Cup'
+    web = f'https://web.archive.org/web/20221115040351/https://en.wikipedia.org/wiki/{year}_FIFA_World_Cup'
     response = requests.get(web)
     content = response.text
     soup = BeautifulSoup(content, 'lxml')
