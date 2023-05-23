e02cc45444913b4c40f8ee4cc8d2dc0eaeddf44c
ian-parra <24ian2002@gmail.com>
2023-05-23 11:25:43 -0300

    Initial commit

--- 1.get_missing_data.py
+++ 1.get_missing_data.py
@@ -3,16 +3,17 @@ from selenium.webdriver.chrome.service import Service
 import time
 import pandas as pd
 
-path ='E:\chromedriver_wepdriver'
+path = r'E:\chromedriver_wepdriver\chromedriver.exe'
 service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service)
 
 
-def get_misssing_data(year):
+def get_missing_data(year):
     web = f'https://en.wikipedia.org/wiki/{year}_FIFA_World_Cup'
 
     driver.get(web)
-    matches = driver.find_elements(by='xpath', value='//td[@align="right"]/.. | //td[@style="text-align:right;"]/..') #extraccion de datos de partidos
+    matches = driver.find_elements(by='xpath',
+                                   value='//td[@align="right"]/.. | //td[@style="text-align:right;"]/..')  # extraccion de datos de partidos
     # matches = driver.find_elements(by='xpath', value='//tr[@style="font-size:90%"]')
 
     Local = []
@@ -21,7 +22,6 @@ def get_misssing_data(year):
     Estadio = []
 
     for match in matches:
-
         Local.append(match.find_element(by='xpath', value='./td[1]').text)
         Resultado.append(match.find_element(by='xpath', value='./td[2]').text)
         Visitante.append(match.find_element(by='xpath', value='./td[3]').text)
@@ -38,7 +38,7 @@ years = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970, 1974,
          1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014,
          2018]
 
-fifa = [get_misssing_data(year) for year in years]
+fifa = [get_missing_data(year) for year in years]
 driver.quit()
 df_fifa = pd.concat(fifa, ignore_index=True)
 df_fifa.to_csv("fifa_worldcup_missing_data.csv", index=False)
