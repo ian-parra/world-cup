f47b1ebf462e2c37e0e18ca596cb53a7e4f019b4
ian-parra <24ian2002@gmail.com>
2023-05-24 10:12:47 -0300

    Initial commit

--- world_cup_extraction.py
+++ world_cup_extraction.py
@@ -2,14 +2,15 @@ import pandas as pd
 from string import ascii_uppercase as abecedario
 import pickle
 
-todas_Tablas = pd.read_html('https://web.archive.org/web/20221115040351/https://en.wikipedia.org/wiki'
-                           '/2022_FIFA_World_Cup')
+todas_Tablas = pd.read_html(
+    'https://web.archive.org/web/20221115040351/https://en.wikipedia.org/wiki''/2022_FIFA_World_Cup')
+
 dict_table = {}
-for letra, i in zip(abecedario, range(12, 68, 7)):
+for letter, i in zip(abecedario, range(12, 68, 7)):
     df = todas_Tablas[i]
     df.rename(columns={df.columns[1]: 'Team'}, inplace=True)
     df.pop('Qualification')
-    dict_table[f'Grupo{letra}'] = df
+    dict_table[f'Group {letter}'] = df
 
 with open('data/dict_table', 'wb') as output:
     pickle.dump(dict_table, output)
